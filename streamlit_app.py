63a4beec1e87fc48c4ea6d4eade136a0d71c6e50
Pau <paula_agallegor@soy.sena.edu.co>
2025-12-19 20:02:29 -0500

    Actualizar streamlit_app.py

--- streamlit_app.py
+++
@@ -1,3 +0,0 @@
-# Nota: Esta es una versión React. Para Streamlit necesitas 
-# convertirla. ¿Prefieres que te haga la versión en Streamlit
-# o te sirve esta versión web que puedes hostear en GitHub Pages?
